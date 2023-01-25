f515e8d59a2ea6390479ad4fc9ef17cd6486db3c
Mohammed Al-Sadi - iMac <ipqtr@hotmail.com>
2023-01-25 08:40:28 +0300

    init

--- nuxt3-cart/app.vue
+++ nuxt3-cart/app.vue
@@ -1,16 +1,9 @@
 <template>
   <v-app id="inspire" :theme="useCartStore().getTheme">
-    <!-- Header -->
-    <Header />
-    <v-main>
-      <v-container>
-        <RouterView />
-      </v-container>
-    </v-main>
+
   </v-app>
 </template>
 
 <script setup>
-import { useCartStore } from './stores/cart';
 
 </script>
